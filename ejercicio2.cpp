8cc4ad7af1ed5be7feb97e8849bace3ab746eec1
jllanquev <jllanquev@unjbg.edu.pe>
2025-05-29 18:50:21 -0500

    DIA2: Se le agrego a la calculadora estetica para que sea un poco mas facil de entender

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -11,20 +11,30 @@ void Division (int a, int b);
 
 int main(){
 	int num1,num2,producto,suma;
+	cout<<"************************************************************"<<endl;
+	cout<<"                        CALCULADORA                                 ";
+	cout<<"\n************************************************************"<<endl;
+	
 	cout<<"Ingrese el primer numero: ";
 	cin>>num1;
 	cout<<"Ingrese el segundo numero: ";
 	cin>>num2;
 	
+	cout<<endl;
+	
+	cout<<"*---------------SUMA-----------------*"<<endl;
 	suma=Suma(num1,num2);
 	cout<<"La suma de "<<num1<<" + "<<num2<<" es: "<<suma;
 	
+	cout<<"\n\n*---------------RESTA-----------------*";	
 	Resta(num1,num2);
 	Resta(num2,num1);
 	
+	cout<<"\n\n*---------------MULTIPLICACION-----------------*";	
 	producto=Multiplicacion(num1,num2);
 	cout<<"\nEl producto de "<<num1<<" * "<<num2<<" es: "<<producto;	
-	
+
+	cout<<"\n\n*---------------DIVISION-----------------*";	
 	Division(num1,num2);
 	Division(num2,num1);
 	
