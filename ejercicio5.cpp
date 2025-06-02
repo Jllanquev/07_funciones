1f61e6fc71b654ad4909a92c5533041f6ba757e5
jllanquev <jllanquev@unjbg.edu.pe>
2025-06-01 19:48:48 -0500

    DIA5: Se creo un programa que calcula el MCD y el MCM

---
+++ ejercicio5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int MaxComunDiv (int a, int b);
+
+int MinComunMult(int a, int b);
+
+int main(){
+	int Mcd,num1,num2,Mcm;
+	cout<<"Ingrese el primer numero: ";
+	cin>>num1;
+	cout<<"Ingrese el segundo numero: ";
+	cin>>num2;
+	
+	Mcd=MaxComunDiv(num1,num2);
+	Mcm=MinComunMult(num1,num2);
+	cout<<"El MCD de "<<num1<<" y "<<num2<<" es: ";
+	cout<<Mcd<<endl;
+	cout<<"El MCM de "<<num1<<" y "<<num2<<" es: ";
+	cout<<Mcm;
+	
+	return 0;
+}
+
+int MaxComunDiv (int a, int b){
+	int re;
+	
+	while(b!=0){
+		re=a%b;
+		a=b;
+		b=re;
+	}
+	
+	return a;
+}
+
+int MinComunMult(int a, int b){
+	int MCM;
+	
+	MCM=(a*b)/MaxComunDiv(a,b);
+	
+	return MCM;
+}
