986bb3b0b8b8abf1110ebf2545c9fe4cb39ceb3c
jllanquev <jllanquev@unjbg.edu.pe>
2025-06-02 20:48:26 -0500

    DIA8: Elaborar una funcion donde calcule una dicha suma para un valor n ingresado

---
+++ ejercicio8.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int Factorial (int b);
+
+double suma (double a);
+
+int main(){
+	int n; 
+	
+	cout<<"Ingrese el valor de n: ";
+	cin>>n;
+	
+	float Suma=suma(n);
+	
+	cout<<"La suma es: "<<Suma;
+	
+	return 0;
+}
+
+int Factorial (int b){
+	int F;
+	F=1;
+	for(int i=1;i<=b;i++){
+		F=F*i;
+	}
+	return F;
+}
+
+double suma (double a){
+	double S=0.0;
+	
+	for(int i=1;i<=a;i++){
+		S=S+(Factorial(i)/(2.0*i));
+	}
+	return S;
+}
